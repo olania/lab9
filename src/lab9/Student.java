3d9f91ed4209b1017e3a64c244578a2f13eac3d5
Anna <olejnikanuta@gmail.com>
2019-01-07 11:31:41 +0200

    + HashSet

--- src/lab9/Student.java
+++ src/lab9/Student.java
@@ -2,6 +2,7 @@ package lab9;
 
 import java.util.Date;
 import java.util.Vector;
+import java.util.HashSet;
 import java.util.concurrent.ThreadLocalRandom;
 
 enum EyeColor {
@@ -33,7 +34,7 @@ class DefaultValues {
 class Student {
 	private static final String[] LASTNAMES = { "Goetsch", "Risinger", "Hemsley", "Desjardin", "Corpuz", "Babineau",
 			"Eilers", "Critelli", "Mascarenas", "Albaugh", "Tichelour", "Gram", "Eslinger", "Keesee", "Pion", "Bortle",
-			"Dick", "Mantle", "Heckel", "Dollar" };
+			"Duck", "Mantle", "Heckel", "Dollar" };
 
 	int idNumber;
 	String lastName;
@@ -69,21 +70,54 @@ class Student {
 		for (int i = 0; i < 20; i++) {
 			Student st = new Student(randomValue(0, 200), LASTNAMES[randomValue(0, 20)], EyeColor.brown,
 					randomValue(80, 260));
-			System.out.print(i + "Student #" + st.idNumber + " name: " + st.lastName + ", Eye Color: " + st.eyeColor + " height: " + st.height + " isOK: ");
+			System.out.print(i + ") " + "Student id#" + st.idNumber + " name: " + st.lastName + ", Eye Color: " + st.eyeColor + " height: " + st.height + " isOK: ");
 			System.out.println(st.isOK);
-
+		
 			if (st.isOK)
 				studentVector.add(st);
+		}
+			HashSet studentHashSet = new HashSet();
+			System.out.println("Filling HashSet with:");
+			for (int j = 0; j < 20; j++) {
+//				Student st1 = new Student(randomValue(0, 200), LASTNAMES[randomValue(0, 20)], EyeColor.brown,
+//						randomValue(80, 260));
+//				System.out.print(i + "Student #" + st1.idNumber + " name: " + st1.lastName + ", Eye Color: " + st1.eyeColor + " height: " + st1.height + " isOK: ");
+//				System.out.println(st1.isOK);
+
+		//		if (st.isOK)
+		//			studentHashSet.add(st);
+				
 		}
 		// System.out.println(LASTNAMES[randomValue(0,20)]);
 		// System.out.println(LASTNAMES[randomValue(0,20)]);
 		// System.out.println(LASTNAMES[randomValue(0,20)]);
 		// System.out.println();
-		for (int i = 0; i < studentVector.size(); i++) {
-			Student st = (Student) studentVector.get(i);
-			System.out.println("Student #" + st.idNumber + " name: " + st.lastName + " height: " + st.height);
+		for (int k = 0; k < studentVector.size(); k++) {
+			Student st1 = (Student) studentVector.get(k);
+//			System.out.println("Student #" + st1.idNumber + " name: " + st1.lastName + " height: " + st1.height);
+			System.out.print(k + ") " + "Student id#" + st1.idNumber + " name: " + st1.lastName + ", Eye Color: " + st1.eyeColor + " height: " + st1.height + " isOK: ");
+			System.out.println(st1.isOK);
+			if (st1.isOK)
+				studentHashSet.add(st1);
 		}
 		// System.out.println((String) ((Student)
 		// studentVector.get(randomValue(0, 20))).lastName);
+//		public static void main(String[] args) {
+//			HashSet studentHashSet = new HashSet();
+//			System.out.println("Filling HashSet with:");
+//			for (int i = 0; i < 20; i++) {
+//				Student st = new Student(randomValue(0, 200), LASTNAMES[randomValue(0, 20)], EyeColor.brown,
+//						randomValue(80, 260));
+//				System.out.print(i + "Student #" + st.idNumber + " name: " + st.lastName + ", Eye Color: " + st.eyeColor + " height: " + st.height + " isOK: ");
+//				System.out.println(st.isOK);
+//
+//				if (st1.isOK)
+//					studentHashSet.add(st1);
+		}
+//			Vector studentHashSet = null;
+//			for (int j = 0; j < studentHashSet.size(); j++) {
+//				Student st1 = (Student) studentHashSet.get(j);
+//				System.out.println("Student #" + st1.idNumber + " name: " + st1.lastName + " height: " + st1.height);
+//			}
 	}
-}
+
